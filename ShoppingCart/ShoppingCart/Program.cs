7436154ee6315dbfab82a6d3ebccb41c617cd0f1
Britney22 <102727336+Britney22@users.noreply.github.com>
2022-04-29 12:29:29 +0200

    added code

---
+++ ShoppingCart/ShoppingCart/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using ShoppingLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace ShoppingCartApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cart = new ShoppingCart();
+
+
+            ShoppingCartItem shoppingCartItem = new ShoppingCartItem("xBox", 3950.99);
+
+            cart.AddShoppingItem(shoppingCartItem);
+
+            shoppingCartItem = new ShoppingCartItem("PS5", 15999.49);
+            cart.AddShoppingItem(shoppingCartItem);
+
+            shoppingCartItem = new ShoppingCartItem("Samsung A50 ", 5000);
+            cart.AddShoppingItem(shoppingCartItem);
+
+            shoppingCartItem = new ShoppingCartItem("Huawei P40", 5500);
+            cart.AddShoppingItem(shoppingCartItem);
+
+
+            List<ShoppingCartItem> items = cart.GetShoppingCartItems();
+            foreach (ShoppingCartItem item in items)
+            {
+                Console.Write(item.Name + "\t\t");
+                Console.WriteLine(item.Price);
+            }
+
+
+            Console.WriteLine("Sub Total: \t" + cart.GetSubTotal());
+            Console.WriteLine("With VAT: \t" + cart.GetVat());
+
+        }
+
+    }
+}
---
+++ ShoppingCart/ShoppingLibary/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLibrary
+{
+    public class ShoppingCartItem //animals
+    {
+        //variables for the name of item and price
+        public string _name;
+        public double _price;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+        }
+
+
+        public ShoppingCartItem(string name, double price)
+        {
+            //create the item and price//Flexible code so that it can take any item in and a price
+            _name = name;
+            _price = price;
+        }
+    }
+}
